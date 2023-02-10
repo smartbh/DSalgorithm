0d5d53e10c60bf2232b04c75a32aba7688d251a1
Nick" Byung heon Lee <43138813+smartbh@users.noreply.github.com>
2023-02-11 05:02:18 +0900

    hash

--- ch11/hash_chain.cpp
+++ ch11/hash_chain.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
 #include <vector>
 #include <list>
 
@@ -10,23 +12,40 @@ class hash_set
 {
     private:
         int sz;  
-        std::vector<int> data;
+        std::vector<std::list<int>> data;
+        //만약 리스트가 아니라 key, value를 쌍으로 갖는 해시=맵 구조로 만들고싶으면
+        //std::pair를 사용하면된다.
+        // ex) 
+        //std::vector<std::list<std::pair<std::string, int>>> data;
+        //이 방식이 hash map 구조, 현재는 hash_set 구조
     public:
-        hash_set(int n) : sz(n), data(sz, -1) {}
+        hash_set(int n) : sz(n), data(sz) {} //생성자
         int hash(int key) {return key % sz;} //키를 해시값으로 변경하는 내용, 이번 해시함수는 키값을 사이즈만큼 나누는것
+        
 
-        void insert(int value) {data[hash(value)] = value;} 
-        bool find(int value) { return (data[hash(value)] == value);}
-        void erase(int value) {data[hash(value)] = -1;}
-
-        void print()
+        void insert(int value) {data[hash(value)].push_back(value);} 
+        //bool find(int value) { return (data[hash(value)] == value);} 
+        bool find(int value) 
         {
-            for(auto n : data)
-            {
-                std::cout << n << " ,";
-            }
-            std::cout << std::endl;
+            auto& entries = data[hash(value)];
+            return std::find(entries.begin(), entries.end(), value) != entries.end();
         }
+        //void erase(int value) {data[hash(value)] = -1;}
+        void erase(int value)
+        {
+            auto& entries = data[hash(value)];
+            auto it = std::find(entries.begin(), entries.end(), value);
+            if(it != entries.end())
+                entries.erase(it);
+        }
+
+        //void print()
+        //{
+        //    for(auto n : data)
+        //    {
+        //    }
+        //    std::cout << std::endl;
+        //}
 };
 
 int main()
@@ -39,6 +58,7 @@ int main()
     num_set.insert(20); // 6
     num_set.insert(999); // 5
     num_set.insert(300); // 6, 20을 300이 덮어씌워 버렸다. << 충돌현상
+    num_set.insert(200); // 4
     //충돌 현상을 해결하기 위해서 체이닝, 오픈 어드레싱 같은 방법을 사용한다.
 
     /*
@@ -56,14 +76,14 @@ int main()
     else
         std::cout << value << " is not found!" << std::endl;
 
-    value = 200;
+    value = 300;
 
     if(num_set.find(value))
         std::cout << value << " is found!" << std::endl;
     else
         std::cout << value << " is not found!" << std::endl;
 
-    num_set.print();
+    //num_set.print();
 
     return 0;
 }
